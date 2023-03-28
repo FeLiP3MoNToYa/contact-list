fd5f4276f2cd66f35b85a0523a9358b4dabcae14
Juan Felipe Montoya Restrepo <pipeldp07@gmail.com>
2023-03-27 23:17:41 -0500

    Agregar1

--- CrearyEliminar.js
+++ CrearyEliminar.js
@@ -8,13 +8,47 @@ let Contacto1 = {
         dirreccion: 'Cll 56 # 9A - 27',
     }
 }
-let Contacto2= {
-    id: 002,
-    nombre: 'Raquel',
-    apellidos: 'Uribe',
-    telefono: '3201459875',
-    ubicaciones: {
-        cuidad: 'Cali',
-        dirreccion: 'Cll 56 # 78 - 11 Int 78',
+let Contacto2 = {
+    Id: 002,
+    Nombre: 'Raquel',
+    Apellidos: 'Uribe',
+    Telefono: '3201459875',
+    Ubicaciones: {
+        Cuidad: 'Cali',
+        Dirreccion: 'Cll 56 # 78 - 11 Int 78',
+    }
+}
+let Contacto3 = {
+    Id: 003,
+    Nombre: 'Andres',
+    Apellidos: 'Tamayo',
+    Telefono: '3745213685',
+    Ubicaciones: {
+        Cuidad: 'Medellin',
+        Dirreccion: 'Cll 10 # 45 - 13',
+    }
+}
+
+let ListaContactos = [Contacto1,Contacto2,Contacto3];
+
+
+function Agregar(Id,Nombre,Apellidos,Telefono,Cuidad,Direccion){
+    let contacto ={
+        Id,
+        Nombre,
+        Apellidos,
+        Telefono,
+        Ubicaciones: {
+            Cuidad,
+            Dirreccion,
+        }
+    }
+    ListaContactos.push(contacto);
+}
+function borrarContactoID(id){
+    for(let i = 0; i < ListaContactos.length;i++){
+        if (ListaContactos[i].id === id) {
+            ListaContactos.splice(i,1);
+        }
     }
 }
