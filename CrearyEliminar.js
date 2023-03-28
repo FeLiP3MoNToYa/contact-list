471bcd8fe944b98b42752c3a56236313a4ec58e2
Juan Felipe Montoya Restrepo <pipeldp07@gmail.com>
2023-03-27 23:11:56 -0500

    Contacto2

---
+++ CrearyEliminar.js
@@ -0,0 +1,20 @@
+let Contacto1 = {
+    Id: 001,
+    Nombre: 'Antonia',
+    Apellidos: 'Restrepo',
+    Telefono: '3105789426',
+    ubicaciones: {
+        Cuidad: 'Barranquilla',
+        dirreccion: 'Cll 56 # 9A - 27',
+    }
+}
+let Contacto2= {
+    id: 002,
+    nombre: 'Raquel',
+    apellidos: 'Uribe',
+    telefono: '3201459875',
+    ubicaciones: {
+        cuidad: 'Cali',
+        dirreccion: 'Cll 56 # 78 - 11 Int 78',
+    }
+}
